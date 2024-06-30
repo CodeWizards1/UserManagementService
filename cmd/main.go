6c8f9ed3cdcf0dbfbd2b0c0ac8f6dd3929342e78
abdulazizax <abdulazizax2001@gmail.com>
2024-06-30 13:59:11 +0500

    third commit

---
+++ cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"userManagement/config"
+	pb "userManagement/genproto/UserManagementSevice/user"
+	"userManagement/services"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+)
+
+func GetDB(path string) (*sqlx.DB, error) {
+	cfg := config.Load(path)
+
+	psqlUrl := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.Postgres.DbHost,
+		cfg.Postgres.DbPort,
+		cfg.Postgres.DbUser,
+		cfg.Postgres.DbPassword,
+		cfg.Postgres.DbName,
+	)
+
+	db, err := sqlx.Connect("postgres", psqlUrl)
+	return db, err
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatal("Failed to listen: ", err)
+	}
+
+	gprcServer := grpc.NewServer()
+
+	db, err := GetDB(".")
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	userManaementService := services.NewUserManagementRepo(db)
+	pb.RegisterUserManagementServiceServer(gprcServer, userManaementService)
+
+	log.Println("gRPC server is running on port 50051")
+	if err := gprcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
--- services/userManagement.go
+++ services/userManagement.go
@@ -1,1 +1,37 @@
 package services
+
+import (
+	"context"
+	pb "userManagement/genproto/UserManagementSevice/user"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type userManagementRepo struct {
+	db *sqlx.DB
+	pb.UnimplementedUserManagementServiceServer
+}
+
+func NewUserManagementRepo(db *sqlx.DB) *userManagementRepo {
+	return &userManagementRepo{db: db}
+}
+
+func (u *userManagementRepo) GetUserById(ctx context.Context, in *pb.IdUserRequest) (*pb.UserResponse, error) {
+	return nil, nil
+}
+
+func (u *userManagementRepo) UpdateUserById(ctx context.Context, in *pb.UpdateUserRequest) (*pb.UserResponse, error) {
+	return nil, nil
+}
+
+func (u *userManagementRepo) DeleteUserById(ctx context.Context, in *pb.IdUserRequest) (*pb.DeleteUserResponse, error) {
+	return nil, nil
+}
+
+func (u *userManagementRepo) GetUserProfile(ctx context.Context, in *pb.IdUserRequest) (*pb.UserProfileResponse, error) {
+	return nil, nil
+}
+
+func (u *userManagementRepo) UpdateUserProfile(ctx context.Context, in *pb.UserProfileRequest) (*pb.UserProfileResponse, error) {
+	return nil, nil
+}
