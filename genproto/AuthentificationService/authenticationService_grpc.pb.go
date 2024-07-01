f3dc713dbbc48a04f80cdbd2b694533d43eb517f
Javohir-A <abdusamatovjavohir@gmail.com>
2024-07-02 02:16:49 +0500

    resolved all issues with submodules

--- genproto/AuthentificationService/authenticationService_grpc.pb.go
+++ genproto/AuthentificationService/authenticationService_grpc.pb.go
@@ -4,7 +4,7 @@
 // - protoc             v3.12.4
 // source: AuthenticationSevice/authenticationService.proto
 
-package authentication
+package AuthentificationService
 
 import (
 	context "context"
